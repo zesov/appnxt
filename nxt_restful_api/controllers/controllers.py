cc4efe4d5cf2befa112c380daade382c84bd4d0a
zesov <jack@Work-MacBook-Air.local>
2018-01-22 14:05:07 +0800

    update restful api

--- nxt_restful_api/controllers/controllers.py
+++ nxt_restful_api/controllers/controllers.py
@@ -4,7 +4,7 @@
 
 import odoo
 from odoo import api, http, SUPERUSER_ID, _
-from odoo.http import Root
+from odoo.http import request
 import werkzeug
 import base64
 import time
@@ -20,107 +20,6 @@ _logger = logging.getLogger(__name__)
 # Odoo Nxt Restful API Method.
 #
 ###############################
-import _ast
-import re
-import csv
-try:                    # Python 3
-    import configparser
-    from threading import current_thread
-    from xmlrpc.client import Fault, ServerProxy, MININT, MAXINT
-    PY2 = False
-except ImportError:     # Python 2
-    import ConfigParser as configparser
-    from threading import currentThread as current_thread
-    from xmlrpclib import Fault, ServerProxy, MININT, MAXINT
-    PY2 = True
-
-DOMAIN_OPERATORS = frozenset('!|&')
-_term_re = re.compile(
-    '([\w._]+)\s*'  '(=(?:like|ilike|\?)|[<>]=?|!?=(?!=)'
-    '|(?<= )(?:like|ilike|in|not like|not ilike|not in|child_of))'  '\s*(.*)')
-# Simplified ast.literal_eval which does not parse operators
-def _convert(node, _consts={'None': None, 'True': True, 'False': False}):
-    if isinstance(node, _ast.Str):
-        return node.s
-    if isinstance(node, _ast.Num):
-        return node.n
-    if isinstance(node, _ast.Tuple):
-        return tuple(map(_convert, node.elts))
-    if isinstance(node, _ast.List):
-        return list(map(_convert, node.elts))
-    if isinstance(node, _ast.Dict):
-        return dict([(_convert(k), _convert(v))
-                     for (k, v) in zip(node.keys, node.values)])
-    if hasattr(node, 'value') and str(node.value) in _consts:
-        return node.value         # Python 3.4+
-    if isinstance(node, _ast.Name) and node.id in _consts:
-        return _consts[node.id]   # Python <= 3.3
-    raise ValueError('malformed or disallowed expression')
-
-if PY2:
-    int_types = int, long
-
-    class _DictWriter(csv.DictWriter):
-        """Unicode CSV Writer, which encodes output to UTF-8."""
-
-        def writeheader(self):
-            # Method 'writeheader' does not exist in Python 2.6
-            header = dict(zip(self.fieldnames, self.fieldnames))
-            self.writerow(header)
-
-        def _dict_to_list(self, rowdict):
-            rowlst = csv.DictWriter._dict_to_list(self, rowdict)
-            return [cell.encode('utf-8') if hasattr(cell, 'encode') else cell
-                    for cell in rowlst]
-else:   # Python 3
-    basestring = str
-    int_types = int
-    _DictWriter = csv.DictWriter
-
-def literal_eval(expression, _octal_digits=frozenset('01234567')):
-    node = compile(expression, '<unknown>', 'eval', _ast.PyCF_ONLY_AST)
-    if expression[:1] == '0' and expression[1:2] in _octal_digits:
-        raise SyntaxError('unsupported octal notation')
-    value = _convert(node.body)
-    if isinstance(value, int_types) and not MININT <= value <= MAXINT:
-         raise ValueError('overflow, int exceeds XML-RPC limits')
-    return value
-
-def searchargs(params, kwargs=None, context=None):
-    """Compute the 'search' parameters."""
-
-    if not params:
-        return ([],)
-    domain = params[0]
-    if not isinstance(domain, list):
-        return params
-    for (idx, term) in enumerate(domain):
-        if isinstance(term, basestring) and term not in DOMAIN_OPERATORS:
-            m = _term_re.match(term.strip())
-            if not m:
-                raise ValueError('Cannot parse term %r' % term)
-            (field, operator, value) = m.groups()
-            try:
-                value = literal_eval(value)
-            except Exception:
-                # Interpret the value as a string
-                pass
-            domain[idx] = (field, operator, value)
-    return domain
-
-def issearchdomain(arg):
-    """Check if the argument is a search domain.
-
-    Examples:
-      - ``[('name', '=', 'mushroom'), ('state', '!=', 'draft')]``
-      - ``['name = mushroom', 'state != draft']``
-      - ``[]``
-    """
-    return isinstance(arg, list) and not (arg and (
-        # Not a list of ids: [1, 2, 3]
-        isinstance(arg[0], int_types) or
-        # Not a list of ids as str: ['1', '2', '3']
-        (isinstance(arg[0], basestring) and arg[0].isdigit())))
 
 def no_token():
     rp = {'result': '','success': False,'message':'invalid token!'}
@@ -130,151 +29,50 @@ def json_response(rp):
     headers = {"Access-Control-Allow-Origin": "*"}
     return werkzeug.wrappers.Response(json.dumps(rp,ensure_ascii=False), mimetype='application/json',headers=headers)
 
-def authenticate(token):
-    try:
-        a = 4 - len(token) % 4
-        if a != 0:
-            token += '==' if a == 2 else '='
-        SERVER,db,login,uid,ts = base64.urlsafe_b64decode(str(token).encode()).decode().split(',')
-        if int(ts) + 60*60*24*7*10 < time.time():
-            return False
-        # todo verify
-        # registry = RegistryManager.get(db)
-        odoo.registry(db).check_signaling()
-        cr = odoo.registry(db).cursor()
-        env = api.Environment(cr, int(uid), {})
-    except Exception as e:
-        return str(e)
-    return env
+db = 'v11c-iap'
+cr = odoo.registry(db).cursor()
+env = api.Environment(cr, SUPERUSER_ID, {})
 
 class NxtRestfulApi(http.Controller):
     @http.route([
-        '/api/v1.0/get_token',
+        '/api/v1/get_token',
         ], type='http', auth="none", csrf=False, methods=['POST','GET'])
-    def get_token(self,serv='http://d10.appnxt.com', login=None, password='admin', db='d10', sid=None, success=True, message='',**kw):
-        """ service, app(user/login),secret(password) """
-        try:
-            uid = http.request.session.authenticate(db, login, password)
-        except Exception as e:
-            rp = {'token': '','success':False,'message':str(e)}
-            return json_response(rp)
-        if not uid:
-            rp = {'token': '','success':False,'message':'you are unauthenticated'}
-            return json_response(rp)
-        e = base64.urlsafe_b64encode(','.join([serv,db,login,str(uid),str(int(time.time()))]).encode())
-        rp = {'token': e.decode().replace('=',''),'success':success,'message':message}
+    def token(self, login=None, password='admin', db='odoo'):
+        rp = env['nxt.api'].get_token(request.httprequest.host_url,login,password,db)
         return json_response(rp)
 
     @http.route([
-        '/api/v1.0/<string:model>',
-        '/api/v1.0/<string:model>/<string:ids>'
+        '/api/v1/<string:model>',
+        '/api/v1/<string:model>/<string:ids>'
         ], auth='none', type='http', csrf=False, methods=['GET'])
     def read_objects(self, model=None, ids=None, **kw):
-        success, message,result,count,offset,limit = True,'','',0,0,80
-        token = kw.pop('token')
-        env = authenticate(token)
-        if not env:
-            return no_token()
-        domain = eval(kw.get('filter',"[]"))
-        fields = eval(kw.get('fields',"[]"))
-        offset = int(kw.get('page','1')) - 1
-        limit = int(kw.get('per_page','80'))
-        order = kw.get('order','id')
-
-        # domain = searchargs(domain)
-
-        if ids:
-            ids = map(int,ids.split(','))
-            domain += [('id','in',ids)]
-        try:
-            count = env[model].search_count(domain)
-            result = env[model].search_read(domain,fields,offset*limit,limit,order)
-            model_fields = env[model].fields_get()
-            for r in result:
-                for f in r.keys():
-                    if model_fields[f]['type'] == 'many2one':
-                        if r[f]:
-                            r[f] = {'id': r[f][0],'display_name':r[f][1]}
-                        else:
-                            r[f] = ''
-                    if model_fields[f]['type'] == 'binary' and r[f]:
-                        r[f] = r[f].decode()
-            if ids and result and len(ids) == 1:
-                result = result[0]
-        except Exception as e:
-            result,success,message = '',False,str(e)
-        rp = {'success': success,'message':message, 'result': result,'total':count,'page': offset+1, 'per_page': limit}
+        rp = env['nxt.api'].get(model,ids,kw)
         return json_response(rp)
 
     @http.route([
-        '/api/v1.0/<string:model>',
+        '/api/v1/<string:model>',
         ], auth='none', type='http', csrf=False, methods=['POST'])
-    def create_objects(self, model=None, success=True, message='', **kw):
-        token = kw.pop('token')
-        env = authenticate(token)
-        if not env:
-            return no_token()
-        try:
-            result = env[model].create(kw).id
-        except Exception as e:
-            result,success,message = '',False,str(e)
-        env.cr.commit()
-        env.cr.close()
-        rp = {'result': result,'success': success,'message':message}
+    def create_objects(self, model=None, **kw):
+        rp = env['nxt.api'].post(model,kw)
         return json_response(rp)
 
     @http.route([
-        '/api/v1.0/<string:model>/<string:ids>',
+        '/api/v1/<string:model>/<string:ids>',
         ], auth='none', type='http', csrf=False, methods=['PUT','PATCH'])
-    def update_objects(self, model=None, ids=None, success=True, message='', **kw):
-        token = kw.pop('token')
-        env = authenticate(token)
-        if not env:
-            return no_token()
-        ids = map(int,ids.split(','))
-        try:
-            result = env[model].browse(ids).write(kw)
-        except Exception as e:
-            result,success,message = '',False,str(e)
-        env.cr.commit()
-        env.cr.close()
-        rp = {'result': result,'success': success,'message':message}
+    def update_objects(self, model=None, ids=None, **kw):
+        rp = env['nxt.api'].put(model,ids,kw)
         return json_response(rp)
 
     @http.route([
-        '/api/v1.0/<string:model>/<string:ids>',
+        '/api/v1/<string:model>/<string:ids>',
         ], auth='none', type='http', csrf=False, methods=['DELETE'])
-    def unlink_objects(self, model=None, ids=None, success=True, message='', **kw):
-        token = kw.pop('token')
-        env = authenticate(token)
-        if not env:
-            return no_token()
-        ids = map(int,ids.split(','))
-        try:
-            result = env[model].browse(ids).unlink()
-        except Exception as e:
-            result,success,message = '',False,str(e)
-        env.cr.commit()
-        env.cr.close()
-        rp = {'result': result,}
+    def unlink_objects(self, model=None, ids=None, **kw):
+        rp = env['nxt.api'].delete(model, ids,kw)
         return json_response(rp)
 
     @http.route([
-        '/api/v1.0/<string:model>/call/<string:method>',
+        '/api/v1/<string:model>/call/<string:method>/<string:args>',
         ], auth='none', type='http', csrf=False, methods=['POST','GET'])
-    def call_method(self, model=None, method=None, success=True, message='', **kw):
-        token = kw.pop('token')
-        env = authenticate(token)
-        if not env:
-            return no_token()
-        try:
-            result = eval('env[model].'+method)(kw)
-        except Exception as e:
-            result,success,message = '',False,str(e)
-        env.cr.commit()
-        env.cr.close()
-        response = {'result': result,'success': success,'message':message}
-        if isinstance(result,dict) and result.get('pagination'):
-            response.update(result.pop('pagination'))
-            response.update({'result': result.pop('results')})
-        return json_response(response)
+    def call_method(self, model=None, method=None,args='', **kw):
+        rp = env['nxt.api'].call_method(model, method,eval(args),kw)
+        return json_response(rp)
--- nxt_restful_api/models/models.py
+++ nxt_restful_api/models/models.py
@@ -1,15 +1,82 @@
 # -*- coding: utf-8 -*-
 
+import odoo
 from odoo import models, fields, api
+from odoo.addons.iap.models.iap import jsonrpc, InsufficientCreditError
 
-# class nxt_restful_api(models.Model):
-#     _name = 'nxt_restful_api.nxt_restful_api'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+DEFAULT_ENDPOINT = 'http://iap-service.y11c.appnxt.com'
+class nxt_restful_api(models.Model):
+    _name = 'nxt.api'
+
+    @api.model
+    def get_token(self, serv=None, login=None, password='admin', db='odoo'):
+        user_token = self.env['iap.account'].get('nxt_restful_api')
+        params = {
+            'account_token': user_token.account_token,
+            'kw':{
+                'serv': serv,
+                'login': login,
+                'password': password,
+                'db': db,
+            }
+        }
+        endpoint = self.env['ir.config_parameter'].sudo().get_param('nxt_restful_api.endpoint', DEFAULT_ENDPOINT)
+        return jsonrpc(endpoint + '/odoo/token', params=params)
+
+    @api.model
+    def get(self,model,ids,kw):
+        user_token = self.env['iap.account'].get('nxt_restful_api')
+        endpoint = self.env['ir.config_parameter'].sudo().get_param('nxt_restful_api.endpoint', DEFAULT_ENDPOINT)
+        params = {
+            'account_token': user_token.account_token,
+            'model': model,
+            'ids': ids,
+            'kw': kw
+        }
+        return jsonrpc(endpoint +'/odoo/get',params=params)
+
+    @api.model
+    def post(self,model,kw):
+        user_token = self.env['iap.account'].get('nxt_restful_api')
+        endpoint = self.env['ir.config_parameter'].sudo().get_param('nxt_restful_api.endpoint', DEFAULT_ENDPOINT)
+        params = {
+            'account_token': user_token.account_token,
+            'model': model,
+            'kw': kw
+        }
+        return jsonrpc(endpoint +'/odoo/post',params=params)
+    @api.model
+    def delete(self,model,ids,kw):
+        user_token = self.env['iap.account'].get('nxt_restful_api')
+        endpoint = self.env['ir.config_parameter'].sudo().get_param('nxt_restful_api.endpoint', DEFAULT_ENDPOINT)
+        params = {
+            'account_token': user_token.account_token,
+            'model': model,
+            'ids':ids,
+            'kw': kw
+        }
+        return jsonrpc(endpoint +'/odoo/delete',params=params)
+    @api.model
+    def put(self,model,ids,kw):
+        user_token = self.env['iap.account'].get('nxt_restful_api')
+        endpoint = self.env['ir.config_parameter'].sudo().get_param('nxt_restful_api.endpoint', DEFAULT_ENDPOINT)
+        params = {
+            'account_token': user_token.account_token,
+            'model': model,
+            'ids':ids,
+            'kw': kw
+        }
+        return jsonrpc(endpoint +'/odoo/put',params=params)
+    @api.model
+    def call_method(self,model,method,args,kwargs):
+        user_token = self.env['iap.account'].get('nxt_restful_api')
+        endpoint = self.env['ir.config_parameter'].sudo().get_param('nxt_restful_api.endpoint', DEFAULT_ENDPOINT)
+        params = {
+            'account_token': user_token.account_token,
+            'model': model,
+            'method': method,
+            'args': args,
+            'kw': kwargs
+        }
+        return jsonrpc(endpoint +'/odoo/call_method',params=params)
